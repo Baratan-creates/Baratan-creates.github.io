25f51158082170cd5ae0ba596b44701febc8ecea
Baratan-creates <baratanfaenthand@gmail.com>
2025-03-15 15:15:19 -0500

    Add files via upload

---
+++ Wan-Tea-Tables/0_15.metadata.js
@@ -0,0 +1,33 @@
+all_metadata["0_15"] = {
+  "sui_image_params": {
+    "prompt": "a cat playing in a magical rainbow forest",
+    "negativeprompt": "flashing lights, bad quality video, blurry, pixelated, chaotic, morphing, flickering, video artifacts, distorted textures, shaky camera",
+    "model": "WanX/Text-2-Video/Wan2_1-T2V-1_3B_bf16",
+    "seed": 1711074260,
+    "steps": 25,
+    "cfgscale": 5.0,
+    "textvideoframes": 61,
+    "textvideofps": 24,
+    "textvideoboomerang": true,
+    "textvideoformat": "h264-mp4",
+    "textvideoframeinterpolationmethod": "RIFE",
+    "textvideoframeinterpolationmultiplier": 2,
+    "aspectratio": "16:9",
+    "width": 832,
+    "height": 480,
+    "sigmashift": 4.5,
+    "vaetilesize": 272,
+    "teacachemode": "video only",
+    "teacachethreshold": 0.15,
+    "automaticvae": true,
+    "nopreviews": true,
+    "swarm_version": "0.9.5.2"
+  },
+  "sui_extra_data": {
+    "date": "2025-03-15",
+    "prep_time": "7.28 min",
+    "generation_time": "2.81 min"
+  }
+}
+file_extensions_alt["0_15"] = "mp4"
+fix_video("0_15")
---
+++ Wan-Tea-Tables/data.js
@@ -0,0 +1,78 @@
+rawData = {
+  "title": "Grids/wan-tea-2025-03-15-14-59-55",
+  "description": "",
+  "ext": "png",
+  "min_width": 832,
+  "min_height": 480,
+  "defaults": {
+    "show_descriptions": true,
+    "autoscale": false,
+    "sticky": false,
+    "sticky_labels": true,
+    "x": "teacachethreshold",
+    "y": "teacachethreshold",
+    "x2": "none",
+    "y2": "none"
+  },
+  "will_run": true,
+  "metadata": null,
+  "axes": [
+    {
+      "id": "teacachethreshold",
+      "title": "TeaCache Threshold",
+      "description": "",
+      "values": [
+        {
+          "key": "0",
+          "path": "0",
+          "title": "0",
+          "description": "",
+          "show": true,
+          "params": {
+            "teacachethreshold": "0"
+          }
+        },
+        {
+          "key": "0_1",
+          "path": "0_1",
+          "title": "0.1",
+          "description": "",
+          "show": true,
+          "params": {
+            "teacachethreshold": "0.1"
+          }
+        },
+        {
+          "key": "0_15",
+          "path": "0_15",
+          "title": "0.15",
+          "description": "",
+          "show": true,
+          "params": {
+            "teacachethreshold": "0.15"
+          }
+        },
+        {
+          "key": "0_3",
+          "path": "0_3",
+          "title": "0.3",
+          "description": "",
+          "show": true,
+          "params": {
+            "teacachethreshold": "0.3"
+          }
+        },
+        {
+          "key": "0_5",
+          "path": "0_5",
+          "title": "0.5",
+          "description": "",
+          "show": true,
+          "params": {
+            "teacachethreshold": "0.5"
+          }
+        }
+      ]
+    }
+  ]
+}
